e7888ed83d50200679e31aa42e9c03566708b2e7
Srinivas Rao <savinirs@gmail.com>
2016-07-10 11:44:17 -0700

    enemies don't wiggle back and forth when aligned

--- lib/zmobies/being.ex
+++ lib/zmobies/being.ex
@@ -7,10 +7,7 @@ defmodule Zmobies.Being do
   end
 
   def move_randomly(being) do
-    alteration = case :random.uniform(2) do
-      1 -> -1
-      2 -> 1
-    end
+    alteration = random_direction
 
     case :random.uniform(2) do
       1 -> %{being | row_index: being.row_index + alteration }
@@ -18,13 +15,28 @@ defmodule Zmobies.Being do
     end
   end
 
+  def random_direction do
+    case :random.uniform(2) do
+      1 -> -1
+      2 -> 1
+    end
+  end
+
   def move_away(being, enemy) do
     case :random.uniform(2) do
       1 ->
-        alteration = if being.row_index > enemy.row_index, do: 1, else: -1
+        alteration = cond do
+         being.row_index > enemy.row_index  -> 1
+         being.row_index < enemy.row_index  -> -1
+         being.row_index == enemy.row_index -> random_direction
+       end
         %{being | row_index: being.row_index + alteration }
       2 ->
-        alteration = if being.col_index > enemy.col_index, do: 1, else: -1
+        alteration = cond do
+          being.col_index > enemy.col_index  -> 1
+          being.col_index < enemy.col_index  -> -1
+          being.col_index == enemy.col_index -> random_direction
+        end
         %{being | col_index: being.col_index + alteration }
     end
   end
@@ -32,10 +44,18 @@ defmodule Zmobies.Being do
   def move_towards(being, enemy) do
     case :random.uniform(2) do
       1 ->
-        alteration = if being.row_index > enemy.row_index, do: -1, else: 1
+        alteration = cond do
+         being.row_index > enemy.row_index  -> -1
+         being.row_index < enemy.row_index  -> 1
+         being.row_index == enemy.row_index -> 0
+       end
         %{being | row_index: being.row_index + alteration }
       2 ->
-        alteration = if being.col_index > enemy.col_index, do: -1, else: 1
+        alteration = cond do
+          being.col_index > enemy.col_index  -> -1
+          being.col_index < enemy.col_index  -> 1
+          being.col_index == enemy.col_index -> 0
+        end
         %{being | col_index: being.col_index + alteration }
     end
   end
